c029da93a768ec4846b578697616de946f9f68d0
Louis Pallett <louis.pallett@outlook.com>
2024-02-07 22:50:03 +0000

    Comments/Pseudocode and MemoryCard setup

--- src/js/Components/Cards.jsx
+++ src/js/Components/Cards.jsx
@@ -4,18 +4,51 @@ import { apiData } from "../scripts/api_data";
 export default function Cards() {
     return (
         <div>
-
+            {/* TEMPORARY */}
+            <MemoryCard data={apiData[0]} />
+            {/* TEMPORARY */}
         </div>
     )
 }
 
 function MemoryCard({ data }) {
     return (
-        <div 
-            className="card-wrapper"
-            style={{ backgroundImage: `url(${data.image})`}}
-        >
-
+        <div className="card-wrapper">
+            <div className="character-name">
+                <p>{data.name}</p>
+            </div>
+            <img src={data.image} />
+            <div className="alias">
+                <p>{data.alias}</p>
+            </div>
         </div>
     )
 }
+
+/*
+==================================
+Pseudocode
+==================================
+
+What we need to do is to do is create each memory card based on the prop "data" (this is the apiData[i] from the Cards()
+component). Each memory card will be identical except for its content, which is gets entirely from the API.
+
+The memory card is simple and should be done first.
+
+____________________
+Functionality (onClick)
+
+Ensure that the initial set of memory cards shows first before adding any functionality. After this is completed successfully,
+you can add a onClick function to each card.
+
+    > The onClick can be added to MemoryCard(). It should do the following:
+        // "Base" case 
+        if index of API object (i.e. the card) is already in the selected array (i.e. already clicked)
+            end game
+            show score
+            reset (unmount and remount Cards Component)
+        else
+            add API object to selected array
+            update effect (re order API objects - i.e. shuffle cards and map)
+
+*/
