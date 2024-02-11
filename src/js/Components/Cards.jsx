dda4d651fd944d89a2d553938b359d564790df78
Louis Pallett <louis.pallett@outlook.com>
2024-02-11 10:12:19 +0000

    Add handleClick to begin functionality to add selected cards to array and set state

--- src/js/Components/Cards.jsx
+++ src/js/Components/Cards.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { apiData } from "../scripts/api_data";
 
 export default function Cards() {
+    const [canPlay, setCanPlay] = useState(true);
+
+    const selectedCards = [];
 
     const shuffleCards = (array) => {
         const shuffledArray = [...array];
@@ -12,12 +15,39 @@ export default function Cards() {
         return shuffledArray;
     }
 
+    const handleClick = ({ data }) => {
+        for (let i = 0; i < selectedCards.length; i++) {
+            if  (data.key == selectedCards[i]) {
+                highScore = selectedCards.length;
+                // Because canPlay is changing state, the component here re renders:
+                    // We do want it to do this, but we also want the cards to shuffle onClick...
+                setCanPlay(false);
+                break;
+            }
+        }
+
+        if (canPlay) {
+            selectedCards.push(data.key);
+        }
+        
+        console.log(selectedCards);
+        console.log(canPlay);
+        console.log(selectedCards);
+    }
+
+    useEffect(() => {
+        
+        return () => {
+            
+        }
+    })
+
     const shuffledArray = shuffleCards(apiData);
 
     return (
         <div className="cards-wrapper">
             {shuffledArray.map((apiData, index) => (
-                <div className="card" key={apiData.key}>
+                <div className="card" key={apiData.key} onClick={() => handleClick({ data: apiData })}>
                     <div className="card-alias">
                         <p>{apiData.alias}</p>
                     </div>
@@ -32,31 +62,3 @@ export default function Cards() {
         </div>
     )
 }
-
-/*
-==================================
-Pseudocode
-==================================
-
-What we need to do is to do is create each memory card based on the prop "data" (this is the apiData[i] from the Cards()
-component). Each memory card will be identical except for its content, which is gets entirely from the API.
-
-The memory card is simple and should be done first.
-
-____________________
-Functionality (onClick)
-
-Ensure that the initial set of memory cards shows first before adding any functionality. After this is completed successfully,
-you can add a onClick function to each card.
-
-    > The onClick can be added to MemoryCard(). It should do the following:
-        // "Base" case 
-        if index of API object (i.e. the card) is already in the selected array (i.e. already clicked)
-            end game
-            show score
-            reset (unmount and remount Cards Component)
-        else
-            add API object to selected array
-            update effect (re order API objects - i.e. shuffle cards and map)
-
-*/
