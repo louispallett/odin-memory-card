ef6b812beacf051492ec90fc6921b6ac4ac4854f
Louis Pallett <louis.pallett@outlook.com>
2024-02-13 22:14:24 +0000

    Add favicon

--- src/js/scripts/main.jsx
+++ src/js/scripts/main.jsx
@@ -8,12 +8,9 @@ import Welcome from '../Components/Welcome';
 /*
 JSX/React
 TODO: ADD a winner announcement when the player has a current score of 12
-TODO: ADD some basic instructions for users playing (possibly a start button too? We could render just the instructions and start button 
-      and then change the state to update the whole root part)
 
 Styling/SCSS
 TODO: UPDATE styling on page and add some animations/more interesting colours
-TODO: REPLACE favicon.
 */
 
 function Main() {
